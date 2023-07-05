f4ada00f4641608c41f043522cebe34d8da0df74
Alone-bat <81104977+Alone-bat@users.noreply.github.com>
2023-07-05 11:45:18 +0200

    Update setup.bat

--- Xtermed_V2.01/setup.bat
+++ Xtermed_V2.01/setup.bat
@@ -8,7 +8,7 @@ if %errorlevel% equ 0 (
     echo Python is not installed. Installing...
     
     REM Download and run the Python installer
-    curl -o python_installer.exe https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe
+    curl -o python_installer.exe https://www.python.org/ftp/python/3.10/python.3.10-amd64.exe
     start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
     del python_installer.exe
     
