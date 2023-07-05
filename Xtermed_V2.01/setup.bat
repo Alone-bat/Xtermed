64ee54cb37f25deafedb613f2af32133d2df183c
Alone-bat <81104977+Alone-bat@users.noreply.github.com>
2023-07-05 11:12:43 +0200

    Update setup.bat

--- Xtermed_V2.01/setup.bat
+++ Xtermed_V2.01/setup.bat
@@ -1,5 +1,28 @@
+@echo off
+
+REM Check if Python is installed
+python --version >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Python is already installed.
+) else (
+    echo Python is not installed. Installing...
+    
+    REM Download and run the Python installer
+    curl -o python_installer.exe https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe
+    start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
+    del python_installer.exe
+    
+    echo Python installation complete.
+)
+
+REM Install Python packages from requirements.txt
+echo Installing Python packages...
 python -m pip install -r requirements.txt
-cls
-echo python main.pyc >> start.bat
-start start.bat
-start /b "" cmd /c del "%~f0"&exit /bs
+
+REM Run the main script
+echo Starting the application...
+python main.pyc
+
+REM Delete the batch file
+del "%~f0"
+exit /b
